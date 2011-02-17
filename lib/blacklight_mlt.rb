083410ba2b3546d2247391bab79f69d6f64edbc3
Chris Beer <chris_beer@wgbh.org>
2011-02-17 10:29:06 -0500

    update mlt plugin to work against stock blacklight

--- lib/blacklight_mlt.rb
+++ lib/blacklight_mlt.rb
@@ -10,26 +10,15 @@ module BlacklightMlt
   
   def self.inject!
     Dispatcher.to_prepare do
-      
+
       unless omit_inject[:view_helpers]
-        CatalogController.class_eval do 
-	  include BlacklightMlt::SolrHelperOverride
-	end unless
-         CatalogController.master_helper_module.include?( 
-            BlacklightMlt::SolrHelperOverride
-         )
-        CatalogController.add_template_helper(
-          MltHelper
-         ) unless
-          CatalogController.master_helper_module.include?( 
-            MltHelper
-          )
+        CatalogController.send(:include, BlacklightMlt::SolrHelperOverride) unless CatalogController.include?(BlacklightMlt::SolrHelperOverride)
+        CatalogController.add_template_helper(BlacklightMlt::TemplateHelper) unless CatalogController.master_helper_module.include?(BlacklightMlt::TemplateHelper)
       end
 
-     unless omit_inject[:controller_mixin]
+      unless omit_inject[:controller_mixin]
         CatalogController.send(:include, BlacklightMlt::ControllerOverride) unless CatalogController.include?(BlacklightMlt::ControllerOverride)
       end
-
     end
   end
 
---
+++ lib/blacklight_mlt/template_helper.rb
@@ -0,0 +1,17 @@
+# Meant to be applied on top of Blacklight helpers
+module BlacklightMlt::TemplateHelper
+  def render_more_like_this
+
+  end
+  def get_more_like_this id=nil
+    return [] unless @response['moreLikeThis']
+    doc = nil
+
+    doc ||= @response['moreLikeThis'].first.last if id.nil?
+    doc ||= @response['moreLikeThis'][id] unless @response['moreLikeThis'][id].nil?
+
+    return [] unless doc['docs']
+
+    doc['docs'].map { |d| SolrDocument.new(d) }
+  end
+end
