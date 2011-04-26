8cb085551314a1e8ec861c06bbdf96aa58539df6
Chris Beer <chris_beer@wgbh.org>
2011-04-26 13:57:24 -0400

    rails 3 lazy update

--- lib/blacklight_mlt.rb
+++ lib/blacklight_mlt.rb
@@ -9,16 +9,13 @@ module BlacklightMlt
   def self.omit_inject ; @omit_inject ; end
   
   def self.inject!
-    Dispatcher.to_prepare do
+    unless omit_inject[:view_helpers]
+      CatalogController.send(:include, BlacklightMlt::SolrHelperOverride) unless CatalogController.include?(BlacklightMlt::SolrHelperOverride)
+      CatalogController.helper(BlacklightMlt::TemplateHelper) unless CatalogController._helpers.include?(BlacklightMlt::TemplateHelper)
+    end
 
-      unless omit_inject[:view_helpers]
-        CatalogController.send(:include, BlacklightMlt::SolrHelperOverride) unless CatalogController.include?(BlacklightMlt::SolrHelperOverride)
-        CatalogController.add_template_helper(BlacklightMlt::TemplateHelper) unless CatalogController.master_helper_module.include?(BlacklightMlt::TemplateHelper)
-      end
-
-      unless omit_inject[:controller_mixin]
-        CatalogController.send(:include, BlacklightMlt::ControllerOverride) unless CatalogController.include?(BlacklightMlt::ControllerOverride)
-      end
+    unless omit_inject[:controller_mixin]
+      CatalogController.send(:include, BlacklightMlt::ControllerOverride) unless CatalogController.include?(BlacklightMlt::ControllerOverride)
     end
   end
 
