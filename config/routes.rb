1a9792ae91497b5392e494fa94bb741c15eb8442
Chris Beer <chris_beer@wgbh.org>
2011-09-28 17:17:12 -0400

    /Engine/

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 
 # We want to add a new collection action to Catalog, without over-writing
 # what's already there. This SEEMS to do it. 
-BlacklightMoreLikeThis.routes.draw do |map|  
+BlacklightMoreLikeThis::Engine.routes.draw do |map|  
 end
 
